8c75b72f4e38abc275a4224cd46d7925455a6ee3
CursedToxic <160098130+CursedToxic@users.noreply.github.com>
2025-11-24 15:36:44 +1100

    Email and Date Of Birth
    24/11/2025
    Added field for Email Address and Date of Birth. The only valid entry for the email field is 'test@example.com'. The date of birth does not work. Will add more function to the page later.

--- function.js
+++ function.js
@@ -1,14 +1,17 @@
 let username;
 let password;
+let email;
+let birthdate;
 
 document.getElementById('userSubmit').onclick = function(){
     username = document.getElementById('userInput').value;
+    email = document.getElementById('userEmail');
     password = document.getElementById('userPassword');
-    if (password.value == 'password' && username == 'admin'){
+    if (password.value == 'password' && username == 'admin' && email.value == "test@example.com"){
         window.alert('Login Successful');
         document.getElementById('userLogin').textContent = `Welcome, ${username}`;
     }
     else {
-        window.alert('Test Input: Invalid Username or Password');
+        window.alert('Test Input: Invalid Details, Please Try Again.');
     }
 }
