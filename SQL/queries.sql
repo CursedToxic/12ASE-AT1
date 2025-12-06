5b82197c7edbb286cce03181641e633542c036bc
CursedToxic <160098130+CursedToxic@users.noreply.github.com>
2025-12-06 20:49:01 +1100

    Updated SQL
    06/12/2025
    Realised that ensuring the security of the query was not meant to be separate, and was supposed to be an optimisation for the first four activities.

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,5 +1,6 @@
+-- Create Table --
 CREATE TABLE IF NOT EXISTS RestaurantReservations(
-    RerservationID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ReservationID INTEGER PRIMARY KEY AUTOINCREMENT,
     UserName TEXT NOT NULL,
     UserEmail TEXT NOT NULL,
     ReservationDate DATE NOT NULL,
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS RestaurantReservations(
     CONSTRAINT email_format CHECK (UserEmail LIKE '%@%.%')
 );
 
--- Activity 1 --
+-- Activity 1: Enter Values into Table --
 INSERT INTO RestaurantReservations(UserName, UserEmail, ReservationDate, Time, NumberOfGuests) VALUES
 ('Alice', 'alice@example.com', '2024-09-20', '19:00', 4),
 ('Bob', 'bob@example.com', '2024-09-27', '20:30', 2),
@@ -16,22 +17,18 @@ INSERT INTO RestaurantReservations(UserName, UserEmail, ReservationDate, Time, N
 ('Diana', 'diana@example.com', '2024-09-06', '19:30', 5),
 ('Ethan', 'ethan@example.com', '2024-09-13', '21:00', 2);
 
--- Activity 2 --
+-- Activity 2: Select Rows where time is between 7:00pm-8:00pm --
 SELECT * 
 FROM RestaurantReservations
 WHERE Time BETWEEN '19:00' AND '20:00';
 
--- Activity 3 --
+-- Activity 3: Find Total Number of Guests between 13/9 & 25/9 --
 SELECT SUM(NumberOfGuests) AS TotalGuests
 FROM RestaurantReservations
 WHERE ReservationDate BETWEEN '2024-09-13' AND '2024-09-25';
 
--- Activity 4 -- 
+-- Activity 4: Delete Table -- 
 DROP TABLE RestaurantReservations;
 
--- Activity 5 --
-cursor.execute(
-    "SELECT * FROM RestaurantReservations
-    WHERE Time BETWEEN ? AND ?;"
-    ("19:00", "20:00")
-);
+-- Activity 5: Each Query is Optimised and Free from Potential SQL Injection --
+-- This is because they are hardcoded into the query in Activity 1 -- 
